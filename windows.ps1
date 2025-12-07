72db0c949a7f6a65507d4b5be5d222f9fb7b0413
byeSystem32 <127899288+byeSystem32@users.noreply.github.com>
2025-12-07 15:08:01 -0800

    added windows.ps1

---
+++ windows.ps1
@@ -0,0 +1,9 @@
+Write-Host "Installing Dev Tools (Windows 10/11)..."
+
+Get-Content tools.txt | ForEach-Object {
+    winget install --id $_ --accept-source-agreements --accept-package-agreements
+}
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+
+Write-Host " Windows Dev Environment Installed"
